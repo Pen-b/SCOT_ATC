7059553b5f8807b95cb43fdd1d3ee5cd535a5829
pen-b <b.postance@lboro.ac.uk>
2016-11-11 12:24:34 +0000

    cleaned data conversion, plots for variance

--- script/01_strip.sh
+++ script/01_strip.sh
@@ -1,5 +1,7 @@
 for i in *.csv;
 #do echo $i;
-#do awk '/:00 -|Route/' $i > _$i
-do sed -i 's/ //g' $i;
-done
+#do awk '/:00 -|Route/' $i > _$i # remove unwated header info
+#do sed -i 's/ //g' $i; # remove unwated empty space
+#do sed -i 's/Time,,/Shour,Ehour,/g' $i; done
+#do sed -i 's/-/,/g' $i; done
+#do sed -i 's/:00,,/:00,/g' $i;done
